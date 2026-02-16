7d4a9ddf77019b0815d5eeac9875fabf2799ca1b
LAPTOP-QA62HH1U\saimo <saimohanmajoju@gmail.com>
2026-02-16 13:10:21 -0500

    first commit

---
+++ src/test/java/com/dragonbyte/tickets/EventTicketPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragonbyte.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
