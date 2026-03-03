13b8d9eadd5e2dac55e94c0be6b1f58ebd823950
LAPTOP-QA62HH1U\saimo <saimohanmajoju@gmail.com>
2026-03-02 22:14:06 -0500

    Implementing Qr code featue

---
+++ src/main/java/com/dragonbyte/tickets/repositories/QrCodeRepository.java
@@ -0,0 +1,11 @@
+package com.dragonbyte.tickets.repositories;
+
+import com.dragonbyte.tickets.domain.entities.QrCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface QrCodeRepository extends JpaRepository<QrCode, UUID> {
+}
